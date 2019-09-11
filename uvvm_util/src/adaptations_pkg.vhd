c347d067dba17f97fbb66816cb34b9d91df1d384
Marius Elvegård <marius.elvegard@bitvis.no>
2019-09-11 19:12:12 +0200

    RAND: added t_randomisation to adaptations_pkg. Added deprecate msg to t_data_source and t_error_injection in adaptations_pkg.

--- uvvm_util/src/adaptations_pkg.vhd
+++ uvvm_util/src/adaptations_pkg.vhd
@@ -244,19 +244,28 @@ package adaptations_pkg is
     others           => ENABLED
   );
 
-  type t_data_source is (     -- May add more types of random ++ later
+  -- Deprecated, will be removed.
+  type t_data_source is (
     NA,
     FROM_BUFFER,
     RANDOM,
     RANDOM_TO_BUFFER
   );
 
-  type t_error_injection is (  -- May add more controlled error injection later
+  -- Deprecated, will be removed.
+  type t_error_injection is (
     NA,
     RANDOM_BIT_ERROR,
     RANDOM_DATA_ERROR,
     RANDOM_ADDRESS_ERROR
   );
+
+  type t_randomisation is (
+    NA,
+    RANDOM,
+    RANDOM_FAVOUR_EDGES
+  );
+
 
   constant C_CMD_IDX_PREFIX : string := " [";
   constant C_CMD_IDX_SUFFIX : string := "]";

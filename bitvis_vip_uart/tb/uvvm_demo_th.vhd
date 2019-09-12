f1eb56ab9382ba9bae15ed62e565d321a8e42646
UVVM <info@bitvis.no>
2019-09-12 18:53:18 +0200

    RAND: updated demo TH with base file with Model and SB.

--- bitvis_vip_uart/tb/uvvm_demo_th.vhd
+++ bitvis_vip_uart/tb/uvvm_demo_th.vhd
@@ -21,10 +21,25 @@ use IEEE.numeric_std.all;
 library uvvm_vvc_framework;
 use uvvm_vvc_framework.ti_vvc_framework_support_pkg.all;
 
+library uvvm_util;
+context uvvm_util.uvvm_util_context;    -- t_channel (RX/TX)
+
 library bitvis_vip_sbi;
+use bitvis_vip_sbi.transaction_pkg.all;
+use bitvis_vip_sbi.vvc_methods_pkg.all;
+use bitvis_vip_sbi.td_vvc_framework_common_methods_pkg.all;
+
 library bitvis_vip_uart;
+use bitvis_vip_uart.transaction_pkg.all;
+use bitvis_vip_uart.vvc_methods_pkg.all;
+use bitvis_vip_uart.td_vvc_framework_common_methods_pkg.all;
+
 library bitvis_uart;
 library bitvis_vip_clock_generator;
+
+library bitvis_vip_scoreboard;
+use bitvis_vip_scoreboard.generic_sb_support_pkg.all;
+
 
 
 -- Test harness entity
@@ -33,6 +48,20 @@ end entity;
 
 -- Test harness architecture
 architecture struct of uvvm_demo_th is
+
+  -- Clock and bit period settings
+  constant C_CLK_PERIOD         : time := 10 ns;
+  constant C_BIT_PERIOD         : time := 16 * C_CLK_PERIOD;
+
+
+  constant C_DATA_WIDTH   : natural := 8;
+  constant C_ADDR_WIDTH   : natural := 3;
+  constant C_SBI_VVC      : natural := 1;
+  constant C_UART_TX_VVC  : natural := 1;
+  constant C_UART_RX_VVC  : natural := 1;
+
+  constant C_ADDR_RX_DATA : unsigned(2 downto 0) := "000";
+
 
   -- DSP interface and general control signals
   signal clk            : std_logic  := '0';
@@ -51,8 +80,9 @@ architecture struct of uvvm_demo_th is
   signal uart_vvc_rx    : std_logic := '1';
   signal uart_vvc_tx    : std_logic := '1';
 
-  constant C_CLK_PERIOD : time    := 10 ns; -- 100 MHz
   constant C_CLOCK_GEN  : natural := 1;
+
+
 
 begin
 
@@ -87,8 +117,8 @@ begin
   -----------------------------------------------------------------------------
   i1_sbi_vvc: entity bitvis_vip_sbi.sbi_vvc
   generic map(
-    GC_ADDR_WIDTH     => 3,
-    GC_DATA_WIDTH     => 8,
+    GC_ADDR_WIDTH     => C_ADDR_WIDTH,
+    GC_DATA_WIDTH     => C_DATA_WIDTH,
     GC_INSTANCE_IDX   => 1
   )
   port map(
@@ -122,6 +152,101 @@ begin
 
   -- Toggle the reset after 5 clock periods
   p_arst: arst <= '1', '0' after 5 *C_CLK_PERIOD;
+
+
+  -----------------------------------------------------------------------------
+  -- Monitor - UART
+  -----------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+  -----------------------------------------------------------------------------
+  -- Model
+  -----------------------------------------------------------------------------
+  p_model: process
+    -- SBI DTT
+    alias sbi_dtt : bitvis_vip_sbi.transaction_pkg.t_transaction_info_group is
+      global_sbi_transaction_info(C_SBI_VVC);
+    -- UART DTT
+    alias uart_rx_dtt : bitvis_vip_uart.transaction_pkg.t_transaction_info_group is
+      global_uart_transaction_info(RX, C_UART_RX_VVC);
+    alias uart_tx_dtt : bitvis_vip_uart.transaction_pkg.t_transaction_info_group is
+      global_uart_transaction_info(TX, C_UART_TX_VVC);
+
+  begin
+
+    while true loop
+
+      -- Wait for DTT trigger
+      wait on sbi_dtt, uart_rx_dtt, uart_tx_dtt;
+
+
+      -------------------------------
+      -- SBI DTT
+      -------------------------------
+      if sbi_dtt.bt'event then
+
+        case sbi_dtt.bt.operation is
+          when WRITE =>
+            if  (sbi_dtt.bt.error_info.delay_error = false) and
+                (sbi_dtt.bt.error_info.write_and_read_error = false) then
+                -- add to UART scoreboard
+                v_uart_sb.add_expected(sbi_dtt.bt.data(C_DATA_WIDTH-1 downto 0));
+            end if;
+
+          when READ =>
+
+
+          when others =>
+            null;
+        end case;
+      end if;
+
+
+      -------------------------------
+      -- UART RX DTT
+      -------------------------------
+      if uart_rx_dtt.bt'event then
+        -- Send to SB is handled by RX VVC.
+        null;
+      end if;
+
+
+      -------------------------------
+      -- UART TX DTT
+      -------------------------------
+      if uart_tx_dtt.bt'event then
+
+        case uart_tx_dtt.bt.operation is
+          when TRANSMIT =>
+            if  (uart_tx_dtt.bt.error_info.parity_bit_error = false) and
+                (uart_tx_dtt.bt.error_info.stop_bit_error = false) then
+
+                -- Add to UART scoreboard
+                v_sbi_sb.add_expected(uart_tx_dtt.bt.data(C_DATA_WIDTH-1 downto 0));
+                -- Wait for UART Transmit to finish
+                insert_delay(SBI_VVCT, 1, 12*C_BIT_PERIOD, "");
+                -- Request SBI Read
+                sbi_read(SBI_VVCT, 1, C_ADDR_RX_DATA, TO_SB, "SBI_READ");
+
+            end if;
+
+          when others =>
+            null;
+
+        end case;
+      end if;
+
+    end loop;
+    wait;
+  end process p_model;
+
+
 
   -----------------------------------------------------------------------------
   -- Clock Generator VVC

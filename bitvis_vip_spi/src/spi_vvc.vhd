721062d32c84e41383cc5b2957adff9c0d951f60
Marius Elvegård <marius.elvegard@inventas.no>
2019-10-24 10:45:50 +0200

    Updated SPI VVC with activity watchdog.

--- bitvis_vip_spi/src/spi_vvc.vhd
+++ bitvis_vip_spi/src/spi_vvc.vhd
@@ -23,6 +23,9 @@ context uvvm_util.uvvm_util_context;
 library uvvm_vvc_framework;
 use uvvm_vvc_framework.ti_vvc_framework_support_pkg.all;
 
+library bitvis_vip_scoreboard;
+use bitvis_vip_scoreboard.generic_sb_support_pkg.all;
+
 use work.spi_bfm_pkg.all;
 use work.vvc_methods_pkg.all;
 use work.vvc_cmd_pkg.all;
@@ -72,6 +75,8 @@ architecture behave of spi_vvc is
   alias vvc_config       : t_vvc_config is shared_spi_vvc_config(GC_INSTANCE_IDX);
   alias vvc_status       : t_vvc_status is shared_spi_vvc_status(GC_INSTANCE_IDX);
   alias transaction_info : t_transaction_info is shared_spi_transaction_info(GC_INSTANCE_IDX);
+  -- Activity Watchdog
+  signal vvc_idx_for_activity_watchdog : integer;
 
 begin
 
@@ -101,6 +106,9 @@ begin
 
     -- initialise shared_vvc_last_received_cmd_idx for channel and instance
     shared_vvc_last_received_cmd_idx(NA, GC_INSTANCE_IDX) := 0;
+    -- Register VVC in activity watchdog register
+    vvc_idx_for_activity_watchdog <= shared_inactivity_watchdog.priv_register_vvc(name      => "SPI",
+                                                                                  instance  => GC_INSTANCE_IDX);
 
     -- Set initial value of v_msg_id_panel to msg_id_panel in config
     v_msg_id_panel := vvc_config.msg_id_panel;
@@ -200,6 +208,14 @@ begin
     variable v_normalized_data_exp                    : t_slv_array(GC_DATA_ARRAY_WIDTH-1 downto 0)(GC_DATA_WIDTH-1 downto 0) := (others => (others => '0'));
     variable v_data_receive                           : t_slv_array(GC_DATA_ARRAY_WIDTH-1 downto 0)(GC_DATA_WIDTH-1 downto 0) := (others => (others => '0'));
 
+    procedure activity_watchdog_register_vvc_state(busy : boolean) is
+      begin
+        shared_inactivity_watchdog.priv_report_vvc_activity(vvc_idx               => vvc_idx_for_activity_watchdog,
+                                                            busy                  => busy,
+                                                            last_cmd_idx_executed => last_cmd_idx_executed);
+        gen_pulse(global_trigger_testcase_inactivity_watchdog, 0 ns, "pulsing global trigger for inactivity watchdog");
+      end procedure;
+  
   begin
     -- 0. Initialize the process prior to first command
     -------------------------------------------------------------------------
@@ -208,9 +224,16 @@ begin
     v_msg_id_panel := vvc_config.msg_id_panel;
 
     loop
+
+      -- Notify activity watchdog
+      activity_watchdog_register_vvc_state(false);
+      
       -- 1. Set defaults, fetch command and log
       -------------------------------------------------------------------------
       work.td_vvc_entity_support_pkg.fetch_command_and_prepare_executor(v_cmd, command_queue, vvc_config, vvc_status, queue_is_increasing, executor_is_busy, C_VVC_LABELS, v_msg_id_panel);
+
+      -- Notify activity watchdog
+      activity_watchdog_register_vvc_state(true);
 
       -- Set the transaction info for waveview
       transaction_info           := C_TRANSACTION_INFO_DEFAULT;

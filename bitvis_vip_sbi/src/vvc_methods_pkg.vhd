be37598e4d5811506394b31ea6d7a626ae988991
Marius Elvegård <marius.elvegard@inventas.no>
2019-10-28 10:09:02 +0100

    Restructured libraries included in SBI VVC methods pkg

--- bitvis_vip_sbi/src/vvc_methods_pkg.vhd
+++ bitvis_vip_sbi/src/vvc_methods_pkg.vhd
@@ -23,14 +23,14 @@ context uvvm_util.uvvm_util_context;
 library uvvm_vvc_framework;
 use uvvm_vvc_framework.ti_vvc_framework_support_pkg.all;
 
+library bitvis_vip_scoreboard;
+use bitvis_vip_scoreboard.generic_sb_support_pkg.all;
+use bitvis_vip_scoreboard.slv_sb_pkg.all;
+
 use work.sbi_bfm_pkg.all;
 use work.vvc_cmd_pkg.all;
 use work.td_target_support_pkg.all;
 use work.transaction_pkg.all;
-
-library bitvis_vip_scoreboard;
-use bitvis_vip_scoreboard.generic_sb_support_pkg.all;
-use bitvis_vip_scoreboard.slv_sb_pkg.all;
 
 
 --=================================================================================================

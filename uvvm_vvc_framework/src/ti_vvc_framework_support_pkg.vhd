b5926064202a588a3469f48dc18d334b670c694b
Marius Elvegård <marius.elvegard@inventas.no>
2019-10-29 12:59:20 +0100

    Changed activity watchdog warning to include leaf VVCs when actual and exp numbers mismatch

--- uvvm_vvc_framework/src/ti_vvc_framework_support_pkg.vhd
+++ uvvm_vvc_framework/src/ti_vvc_framework_support_pkg.vhd
@@ -692,7 +692,7 @@ package body ti_vvc_framework_support_pkg is
       shared_inactivity_watchdog.priv_list_registered_vvc(msg);
       alert(TB_WARNING, "Number of VVCs in activity watchdog is not expected, actual=" & 
                         to_string(shared_inactivity_watchdog.priv_get_num_registered_vvc) & ", exp=" & to_string(num_exp_vvc) & ".\n" & 
-                        "Note that channels are counted individually. " & msg);
+                        "Note that leaf VVCs (e.g. channels) are counted individually. " & msg);
 
     end if;
 

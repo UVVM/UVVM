e1237cc68db281977b322c8a265bcb7619b50605
Marius Elvegård <marius.elvegard@inventas.no>
2019-11-08 11:06:08 +0100

    Release: changed output msg regarding total number of failings tests in regression testing.

--- release/regression_test/run_regression.py
+++ release/regression_test/run_regression.py
@@ -53,7 +53,7 @@ def main():
   num_failing_tests = 0
 
   for module in modules:
-    print("Starting %s. %i failing tests up to now" %(module, num_failing_tests))
+    print("Starting %s. Total number of failing tests: %i." %(module, num_failing_tests))
     cd_to_module(module)
     num_failing_tests += simulate_module(module)
 

4292574ea4412ece1ab2bfba6db9b255f8d0bbf1
Marius Elvegård <marius.elvegard@me.no>
2019-11-20 10:11:46 +0100

    BV_UVVM-789: fixed AXIStream name when calling activity_watchdog() in interpreter

--- bitvis_vip_axistream/src/axistream_vvc.vhd
+++ bitvis_vip_axistream/src/axistream_vvc.vhd
@@ -115,7 +115,7 @@ begin
       -- initialise shared_vvc_last_received_cmd_idx for channel and instance
       shared_vvc_last_received_cmd_idx(NA, GC_INSTANCE_IDX) := 0;
       -- Register VVC in activity watchdog register
-      vvc_idx_for_activity_watchdog <= shared_inactivity_watchdog.priv_register_vvc(name      => "Axistream",
+      vvc_idx_for_activity_watchdog <= shared_inactivity_watchdog.priv_register_vvc(name      => "AXIStream",
                                                                                   instance  => GC_INSTANCE_IDX);
       -- Set initial value of v_msg_id_panel to msg_id_panel in config
       v_msg_id_panel := vvc_config.msg_id_panel;

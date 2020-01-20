e24f1081daca1544c557aa45c537db491acc81db
Marius Elvegård <marius.elvegard@me.no>
2020-01-20 09:37:12 +0100

    BV_UVVM-830: added ID_NEVER to SBI VVC check and added default values in SBI BFM QR

--- bitvis_vip_sbi/src/sbi_vvc.vhd
+++ bitvis_vip_sbi/src/sbi_vvc.vhd
@@ -400,8 +400,9 @@ begin
             -- Delay specified using time
             wait until terminate_current_cmd.is_active = '1' for v_cmd.delay;
           else
-            check_value(vvc_config.bfm_config.clock_period > -1 ns, TB_ERROR, "BFM config clock_period not set", C_SCOPE);
             -- Delay specified using integer
+            check_value(vvc_config.bfm_config.clock_period > -1 ns, TB_ERROR, "Check that clock_period is configured when using insert_delay().",
+                        C_SCOPE, ID_NEVER, vvc_config.msg_id_panel);
             wait until terminate_current_cmd.is_active = '1' for v_cmd.gen_integer_array(0) * vvc_config.bfm_config.clock_period;
           end if;
 

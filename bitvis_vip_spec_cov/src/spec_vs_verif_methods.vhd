fceef4c88048d7b8a915f7a74d0c6d3cddc84472
Marius Elvegård <marius.elvegard@me.no>
2020-01-31 13:41:44 +0100

    SPEC_COV: renamed package to spec_cov_methods, added priv_ prefix to internal methods, removed t_faile_on_alert_mismatch_severity

--- bitvis_vip_spec_cov/src/spec_vs_verif_methods.vhd
+++ bitvis_vip_spec_cov/src/spec_vs_verif_methods.vhd
@@ -24,34 +24,46 @@ use std.textio.all;
 
 use work.csv_file_reader_pkg.all;
 
-package spec_vs_verif_methods is  
+package spec_cov_methods is  
+
   file RESULT_FILE : text;
 
-  -- A requirement will be reported as failed if there is a mismatch on the error counters. 
-  -- This type is used to specify if a mismatch of severity level warning will cause the requirement to fail, or if a mismatch of severity error is needed.
-  type t_fail_on_alert_mismatch_severity is (WARNING, ERROR);
+  -- Starts the requirement coverage process by reading the requirement file and opening the output file
+  procedure initialize_req_cov(
+    variable testcase       : string;
+    constant req_to_tc_map  : string;
+    constant output_file    : string := C_DEFAULT_RESULT_FILE_NAME
+  );
 
   -- Log requirement and testcase to file. 
   -- This function checks the global error mismatch counter for mismatch between expected and observed alerts of severity 'fail_on_alert_mismatch_severity'.
   procedure log_req_cov(
     constant requirement : string;
     constant testcase    : string;
-    constant passed      : boolean := true;
-    constant fail_on_alert_mismatch_severity : t_fail_on_alert_mismatch_severity := ERROR
+    constant test_status : t_test_status := PASS
   );
-  
-  -- Starts the requirement coverage process by reading the requirement file and opening the output file
-  procedure start_req_cov(
-    constant req_to_tc_map  : string;
-    constant output_file    : string := C_DEFAULT_RESULT_FILE_NAME
+  -- Overloading procedure without testcase
+  procedure log_req_cov(
+    constant requirement : string;
+    constant test_status : t_test_status := PASS
+  );
+  -- Overloadding procedure without t_test_status
+  procedure log_req_cov(
+    constant requirement : string;
+    constant testcase    : string
+  );
+  -- Overloading procedure without testcase and t_test_status
+  procedure log_req_cov(
+    constant requirement : string
   );
   
   -- Ends the requirement coverage by writing a check-string to the result file, closing all files and cleaning up the memory.
-  procedure end_req_cov(
+  procedure finalize_req_cov(
     constant VOID : t_void
   );
   
   
+
 --=================================================================================================  
 -- Functions and procedures declared below this line are intended as internal functions
 --=================================================================================================  
@@ -76,102 +88,123 @@ constant C_FAIL_STRING : string := "FAIL";
 constant C_SCOPE       : string := "SPEC_VS_VERIF";
 constant C_REQ_COV_SUCCESSFUL_STRING : string := "Requirement coverage completed successfully";
 
-procedure log_entry(
+procedure priv_log_entry(
   constant index : natural
 );
 
-procedure read_and_parse_csv_file(
+procedure priv_read_and_parse_csv_file(
   constant req_to_tc_map  : string
 );
 
-procedure initialize_result_file(
+procedure priv_initialize_result_file(
   constant file_name : string
 );
 
-impure function requirement_exists(
+impure function priv_requirement_exists(
     requirement : string
 ) return boolean;
 
-impure function requirement_and_tc_exists(
+impure function priv_requirement_and_tc_exists(
     requirement : string;
     testcase    : string
 ) return boolean;
 
-function boolean_to_pass_fail_string(
-    passed : boolean
-) return string;
-
-impure function get_description(
+impure function priv_get_description(
     requirement : string;
     testcase    : string
 ) return string;
 
-end package spec_vs_verif_methods;
+end package spec_cov_methods;
 
 
 --=================================================================================================
 --=================================================================================================
 --=================================================================================================
 
-package body spec_vs_verif_methods is
+package body spec_cov_methods is
+
+  -- This testcase string will be used when no testcase is 
+  -- given when using the log_req_cov() method.
+  variable v_default_testcase : string;
+
+
+  procedure initialize_req_cov(
+    variable testcase       : string;
+    constant req_to_tc_map  : string;
+    constant output_file    : string := C_DEFAULT_RESULT_FILE_NAME
+  ) is
+  begin
+    v_default_testcase := testcase;
+    priv_read_and_parse_csv_file(req_to_tc_map);    
+    priv_initialize_result_file(output_file);
+  end procedure;
+  
+  
 
   procedure log_req_cov(
     constant requirement : string;
     constant testcase    : string;
-    constant passed      : boolean := true;
-    constant fail_on_alert_mismatch_severity : t_fail_on_alert_mismatch_severity := ERROR
+    constant test_status : t_test_status := PASS
   ) is
     variable v_requirement_to_file_line : line;
-    variable v_requirement_passed : boolean := passed;
+    variable v_requirement_status       : t_test_status := test_status;
   begin
     if shared_requirements_in_array = 0 then
-      alert(TB_ERROR, "Requirements not been parsed. Please used start_req_cov() before calling log_req_cov().", C_SCOPE);
+      alert(TB_ERROR, "Requirements not been parsed. Please used initialize_req_cov() before calling log_req_cov().", C_SCOPE);
       return;
     end if;
     
     -- Check if requirement exists
     if shared_req_vs_cov_strict_testcase_checking then
-      check_value(requirement_and_tc_exists(requirement, testcase), C_REQ_TC_MISMATCH_SEVERITY, "Checking that requirement and testcase exists in requirement list", C_SCOPE);
+      check_value(priv_requirement_and_tc_exists(requirement, testcase), C_REQ_TC_MISMATCH_SEVERITY, "Checking that requirement and testcase exists in requirement list", C_SCOPE);
     else
-      check_value(requirement_exists(requirement), C_REQ_TC_MISMATCH_SEVERITY, "Checking that requirement exists in requirement list", C_SCOPE);
+      check_value(priv_requirement_exists(requirement), C_REQ_TC_MISMATCH_SEVERITY, "Checking that requirement exists in requirement list", C_SCOPE);
     end if;
 
-    -- Check if there were any errors globally
-    if (fail_on_alert_mismatch_severity = ERROR) then
-      if (shared_uvvm_status.found_unexpected_simulation_errors_or_worse = 1) then
-        v_requirement_passed := false;
-      end if;
-    else
-      if (shared_uvvm_status.found_unexpected_simulation_warnings_or_worse = 1) then
-        v_requirement_passed := false;
-      end if;
+    ---- Check if there were any errors globally
+    if (shared_uvvm_status.found_unexpected_simulation_warnings_or_worse = 1) then
+      v_requirement_passed := FAILE;
     end if;
 
     -- Log result to transcript
-    log(ID_SPEC_VS_VERIF, "Logging requirement " & requirement & " [" & boolean_to_pass_fail_string(v_requirement_passed) & "]. '" & get_description(requirement, testcase) & "'.", C_SCOPE);
+    log(ID_SPEC_VS_VERIF, "Logging requirement " & requirement & " [" & v_requirement_passed & "]. '" & priv_get_description(requirement, testcase) & "'.", C_SCOPE);
 
     -- Log to file
-    write(v_requirement_to_file_line, requirement & C_CSV_DELIMITER & testcase & C_CSV_DELIMITER & boolean_to_pass_fail_string(v_requirement_passed));
+    write(v_requirement_to_file_line, requirement & C_CSV_DELIMITER & testcase & C_CSV_DELIMITER & v_requirement_passed);
     writeline(RESULT_FILE, v_requirement_to_file_line);
   end procedure;
-    
 
-  procedure start_req_cov(
-    constant req_to_tc_map  : string;
-    constant output_file    : string := C_DEFAULT_RESULT_FILE_NAME
-  ) is
-  begin
-    read_and_parse_csv_file(req_to_tc_map);    
-    initialize_result_file(output_file);
-  end procedure;
+  -- Overloading procedure without testcase
+  procedure log_req_cov(
+    constant requirement : string;
+    constant test_status : t_test_status := PASS
+  );
+  -- Overloadding procedure without t_test_status
+  procedure log_req_cov(
+    constant requirement : string;
+    constant testcase    : string
+  );
+  -- Overloading procedure without testcase and t_test_status
+  procedure log_req_cov(
+    constant requirement : string
+  );
   
-  
-  procedure end_req_cov(
+
+
+
+  --
+  -- finalize_req_cov()
+  --
+  --   This method will free used memory, write the
+  --   run summary line to file and close the file.
+  --
+  procedure finalize_req_cov(
     constant VOID : t_void
   ) is
     variable v_checksum_string : line;
   begin
 
+    -- Free used memory
     log(ID_SPEC_VS_VERIF, "Freeing stored requirements from memory", C_SCOPE);
     for i in 0 to shared_requirements_in_array-1 loop
       deallocate(shared_requirement_array(i).requirement);
@@ -184,6 +217,7 @@ package body spec_vs_verif_methods is
     end loop;
     shared_requirements_in_array := 0;
         
+    -- Add closing line
     log(ID_SPEC_VS_VERIF, "Marking requirement coverage results as successful", C_SCOPE);
     write(v_checksum_string, C_REQ_COV_SUCCESSFUL_STRING);
     writeline(RESULT_FILE, v_checksum_string);
@@ -199,17 +233,8 @@ package body spec_vs_verif_methods is
 -- Functions and procedures declared below this line are intended as internal functions
 --=================================================================================================  
 
-function boolean_to_pass_fail_string(
-    passed : boolean
-  ) return string is
-  begin
-    if passed then
-      return C_PASS_STRING;
-    end if;
-    return C_FAIL_STRING;
-  end;
 
-procedure initialize_result_file(
+procedure priv_initialize_result_file(
   constant file_name : string
 ) is
   variable v_file_open_status : FILE_OPEN_STATUS;
@@ -218,7 +243,7 @@ begin
   check_file_open_status(v_file_open_status, file_name);
 end procedure;
 
-procedure read_and_parse_csv_file(
+procedure priv_read_and_parse_csv_file(
     constant req_to_tc_map  : string
 ) is 
   variable v_tc_valid : boolean;
@@ -226,7 +251,7 @@ begin
   log(ID_SPEC_VS_VERIF, "Reading and parsing requirement to testcase map file, " & req_to_tc_map, C_SCOPE);
 
   if shared_requirements_in_array > 0 then
-    alert(TB_ERROR, "Requirements have already been read from file, please call end_req_cov before starting a new requirement coverage process.", C_SCOPE);
+    alert(TB_ERROR, "Requirements have already been read from file, please call finalize_req_cov before starting a new requirement coverage process.", C_SCOPE);
     return;
   end if;
   shared_csv_file.initialize(req_to_tc_map, C_CSV_DELIMITER);
@@ -252,7 +277,7 @@ begin
     -- Validate entry
     shared_requirement_array(shared_requirements_in_array).valid := true;
 
-    log_entry(shared_requirements_in_array);
+    priv_log_entry(shared_requirements_in_array);
     shared_requirements_in_array := shared_requirements_in_array + 1;
   end loop;
   
@@ -261,7 +286,7 @@ begin
 
 end procedure;
 
-procedure log_entry(
+procedure priv_log_entry(
     constant index : natural
   ) is
   begin
@@ -276,7 +301,7 @@ procedure log_entry(
     end if;
 end procedure;
 
-impure function requirement_exists(
+impure function priv_requirement_exists(
   requirement : string
 ) return boolean is
 begin
@@ -288,15 +313,15 @@ begin
   return false;
 end function;
 
-impure function requirement_and_tc_exists(
+impure function priv_requirement_and_tc_exists(
     requirement : string;
     testcase    : string
 ) return boolean is
 begin
   for i in 0 to shared_requirements_in_array-1 loop
-    if shared_requirement_array(i).requirement.all(1 to requirement'length) = requirement(1 to requirement'length) then
+    if to_upper(shared_requirement_array(i).requirement.all(1 to requirement'length)) = to_upper(requirement(1 to requirement'length)) then
       for tc in 0 to shared_requirement_array(i).num_tcs-1 loop
-        if shared_requirement_array(i).tc_list(tc).all(1 to testcase'length) = testcase(1 to testcase'length) then
+        if to_upper(shared_requirement_array(i).tc_list(tc).all(1 to testcase'length)) = to_upper(testcase(1 to testcase'length)) then
           return true;
         end if;
       end loop;
@@ -305,7 +330,7 @@ begin
   return false;
 end;
 
-impure function get_description(
+impure function priv_get_description(
     requirement : string;
     testcase    : string
 ) return string is
@@ -329,4 +354,4 @@ begin
   return "DESCRIPTION NOT FOUND";
 end;
 
-end package body spec_vs_verif_methods;
+end package body spec_cov_methods;

5b7326129694eed6f7586b49f17dc3715ee76dcb
Marius Elvegård <marius.elvegard@me.no>
2020-02-19 11:26:46 +0100

    BV_UVVM-846: added missing testcase from internal TB to internal_run_spec_cov.py

--- bitvis_vip_spec_cov/internal_script/internal_run_spec_cov.py
+++ bitvis_vip_spec_cov/internal_script/internal_run_spec_cov.py
@@ -14,7 +14,8 @@ test_list = [
             ["python3", "../script/run_spec_cov.py", "--config", "../internal_tb/internal_cfg_1_strict_2.txt"],
             ["python3", "../script/run_spec_cov.py", "--config", "../internal_tb/internal_cfg_2_strict_0.txt"],
             ["python3", "../script/run_spec_cov.py", "--config", "../internal_tb/internal_cfg_2_strict_1.txt"],
-            ["python3", "../script/run_spec_cov.py", "--config", "../internal_tb/internal_cfg_2_strict_2.txt"]
+            ["python3", "../script/run_spec_cov.py", "--config", "../internal_tb/internal_cfg_2_strict_2.txt"],
+            ["python3", "../script/run_spec_cov.py", "--strictness", "0", "-r", "../internal_tb/internal_req_file.csv", "-p", "../sim/pc_10.csv", "-s", "../sim/sc_10.csv"]
             ]
 
 

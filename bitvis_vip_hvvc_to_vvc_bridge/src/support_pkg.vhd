b5eceaab1bf81f303bf9ff396e500c43b7f24185
Erick Castillo <erick.castillo@bitvis.no>
2020-03-02 09:36:41 +0100

    HVVC: Moved some code into procedures in the support_pkg.

--- bitvis_vip_hvvc_to_vvc_bridge/src/support_pkg.vhd
+++ bitvis_vip_hvvc_to_vvc_bridge/src/support_pkg.vhd
@@ -15,6 +15,7 @@
 
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
 library uvvm_util;
 context uvvm_util.uvvm_util_context;
@@ -31,6 +32,10 @@ package support_pkg is
     signal hvvc_to_bridge : out t_hvvc_to_bridge
   );
 
+  procedure bridge_to_hvvc_trigger(
+    signal bridge_to_hvvc : out t_bridge_to_hvvc
+  );
+
   procedure send_to_bridge(
     signal   hvvc_to_bridge            : out t_hvvc_to_bridge;
     constant operation                 : in  t_vvc_operation;
@@ -71,6 +76,19 @@ package support_pkg is
     constant current_byte_idx_in_field : in  natural;
     constant msg_id_panel              : in  t_msg_id_panel;
     constant field_timeout_margin      : in  time
+  );
+
+  procedure get_dut_address_config(
+    constant dut_if_field_config    : in  t_dut_if_field_config_direction_array;
+    signal   hvvc_to_bridge         : in  t_hvvc_to_bridge;
+    variable dut_address            : out unsigned;
+    variable dut_address_increment  : out integer
+  );
+
+  procedure get_data_width_config(
+    constant dut_if_field_config : in  t_dut_if_field_config_direction_array;
+    signal   hvvc_to_bridge      : in  t_hvvc_to_bridge;
+    variable data_width          : out positive
   );
 
 end package support_pkg;
@@ -87,6 +105,16 @@ package body support_pkg is
     wait for 0 ns;
     hvvc_to_bridge.trigger <= false;
   end procedure hvvc_to_bridge_trigger;
+
+  -- Sends a delta cycle pulse in the bridge_to_hvvc.trigger signal
+  procedure bridge_to_hvvc_trigger(
+    signal bridge_to_hvvc : out t_bridge_to_hvvc
+  ) is
+  begin
+    bridge_to_hvvc.trigger <= true;
+    wait for 0 ns;
+    bridge_to_hvvc.trigger <= false;
+  end procedure bridge_to_hvvc_trigger;
 
   -- Send an operation to the bridge using a data array
   procedure send_to_bridge(
@@ -163,5 +191,53 @@ package body support_pkg is
     wait until bridge_to_hvvc.trigger = true;
   end procedure blocking_send_to_bridge;
 
+  -- Returns the DUT address config for a specific field
+  procedure get_dut_address_config(
+    constant dut_if_field_config    : in  t_dut_if_field_config_direction_array;
+    signal   hvvc_to_bridge         : in  t_hvvc_to_bridge;
+    variable dut_address            : out unsigned;
+    variable dut_address_increment  : out integer
+  ) is
+    variable v_direction : t_direction;
+  begin
+    if hvvc_to_bridge.operation = TRANSMIT then -- Expand if other operations
+      v_direction := TRANSMIT;
+    else
+      v_direction := RECEIVE;
+    end if;
+
+    -- If no configs are defined for all fields the last config is used
+    if hvvc_to_bridge.dut_if_field_idx > dut_if_field_config(v_direction)'high then
+      dut_address_increment := dut_if_field_config(v_direction)(dut_if_field_config(v_direction)'high).dut_address_increment;
+      dut_address := dut_if_field_config(v_direction)(dut_if_field_config(v_direction)'high).dut_address +
+        (hvvc_to_bridge.current_byte_idx_in_field*dut_address_increment);
+    else
+      dut_address_increment := dut_if_field_config(v_direction)(hvvc_to_bridge.dut_if_field_idx).dut_address_increment;
+      dut_address := dut_if_field_config(v_direction)(hvvc_to_bridge.dut_if_field_idx).dut_address +
+        (hvvc_to_bridge.current_byte_idx_in_field*dut_address_increment);
+    end if;
+  end procedure get_dut_address_config;
+
+  -- Returns the DUT data width config for a specific field
+  procedure get_data_width_config(
+    constant dut_if_field_config : in  t_dut_if_field_config_direction_array;
+    signal   hvvc_to_bridge      : in  t_hvvc_to_bridge;
+    variable data_width          : out positive
+  ) is
+    variable v_direction : t_direction;
+  begin
+    if hvvc_to_bridge.operation = TRANSMIT then -- Expand if other operations
+      v_direction := TRANSMIT;
+    else
+      v_direction := RECEIVE;
+    end if;
+
+    -- If no configs are defined for all fields the last config is used
+    if hvvc_to_bridge.dut_if_field_idx > dut_if_field_config(v_direction)'high then
+      data_width := dut_if_field_config(v_direction)(dut_if_field_config(v_direction)'high).data_width;
+    else
+      data_width := dut_if_field_config(v_direction)(hvvc_to_bridge.dut_if_field_idx).data_width;
+    end if;
+  end procedure get_data_width_config;
 
 end package body support_pkg;

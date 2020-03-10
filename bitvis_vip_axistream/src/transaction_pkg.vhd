c70a3fba9e788b50b7d1fd521628eb685992b609
Marius Elvegård <marius.elvegard@me.no>
2020-03-10 10:21:10 +0100

    BV_UVVM-860: changed to new t_transaction types for AXI-Stream.

--- bitvis_vip_axistream/src/transaction_pkg.vhd
+++ bitvis_vip_axistream/src/transaction_pkg.vhd
@@ -81,8 +81,8 @@ package transaction_pkg is
     cmd_idx => -1
     );
 
-  -- Transaction
-  type t_transaction is record
+  -- Base transaction
+  type t_base_transaction is record
     operation           : t_operation;
     data_array          : t_byte_array(0 to C_VVC_CMD_DATA_MAX_BYTES-1);
     user_array          : t_user_array(0 to C_VVC_CMD_DATA_MAX_WORDS-1);
@@ -93,7 +93,7 @@ package transaction_pkg is
     transaction_status  : t_transaction_status;
   end record;
 
-  constant C_TRANSACTION_SET_DEFAULT : t_transaction := (
+  constant C_BASE_TRANSACTION_SET_DEFAULT : t_base_transaction := (
     operation           => NO_OPERATION,
     data_array          => (others => (others => '0')),
     user_array          => (others => (others => '0')),
@@ -106,13 +106,11 @@ package transaction_pkg is
 
   -- Transaction group
   type t_transaction_group is record
-    bt : t_transaction;
-    ct : t_transaction;
+    bt : t_base_transaction;
   end record;
 
   constant C_TRANSACTION_GROUP_DEFAULT : t_transaction_group := (
-    bt => C_TRANSACTION_SET_DEFAULT,
-    ct => C_TRANSACTION_SET_DEFAULT
+    bt => C_BASE_TRANSACTION_SET_DEFAULT
     );
 
   -- Global DTT trigger signal

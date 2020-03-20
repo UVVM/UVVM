d1380431409fd76a2dc1c13b903458132d672ba5
Erick Castillo <erick.castillo@bitvis.no>
2020-03-20 13:32:00 +0100

    HVVC: Extended await_completion timeout to handle the time to process a complete packet.

--- bitvis_vip_hvvc_to_vvc_bridge/src/hvvc_to_vvc_bridge_sbi.vhd
+++ bitvis_vip_hvvc_to_vvc_bridge/src/hvvc_to_vvc_bridge_sbi.vhd
@@ -103,7 +103,7 @@ begin
             sbi_write(SBI_VVCT, GC_INSTANCE_IDX, v_dut_address, v_data_slv(v_dut_data_width*(i+1)-1 downto v_dut_data_width*i),
               "Send data over SBI", GC_SCOPE, hvvc_to_bridge.msg_id_panel);
             v_cmd_idx := get_last_received_cmd_idx(SBI_VVCT, GC_INSTANCE_IDX, NA, GC_SCOPE);
-            await_completion(SBI_VVCT, GC_INSTANCE_IDX, v_cmd_idx, GC_PHY_MAX_ACCESS_TIME, "Wait for write to finish.", GC_SCOPE, hvvc_to_bridge.msg_id_panel);
+            await_completion(SBI_VVCT, GC_INSTANCE_IDX, v_cmd_idx, GC_MAX_NUM_WORDS*GC_PHY_MAX_ACCESS_TIME, "Wait for write to finish.", GC_SCOPE, hvvc_to_bridge.msg_id_panel);
             v_dut_address := v_dut_address + v_dut_address_increment;
           end loop;
 
@@ -112,7 +112,7 @@ begin
           for i in 0 to v_num_transfers-1 loop
             sbi_read(SBI_VVCT, GC_INSTANCE_IDX, v_dut_address, "Read data over SBI", TO_RECEIVE_BUFFER, GC_SCOPE, hvvc_to_bridge.msg_id_panel);
             v_cmd_idx := get_last_received_cmd_idx(SBI_VVCT, GC_INSTANCE_IDX, NA, GC_SCOPE);
-            await_completion(SBI_VVCT, GC_INSTANCE_IDX, v_cmd_idx, GC_PHY_MAX_ACCESS_TIME, "Wait for read to finish.", GC_SCOPE, hvvc_to_bridge.msg_id_panel);
+            await_completion(SBI_VVCT, GC_INSTANCE_IDX, v_cmd_idx, GC_MAX_NUM_WORDS*GC_PHY_MAX_ACCESS_TIME, "Wait for read to finish.", GC_SCOPE, hvvc_to_bridge.msg_id_panel);
             fetch_result(SBI_VVCT, GC_INSTANCE_IDX, v_cmd_idx, v_sbi_received_data, "Fetching received data.", TB_ERROR, GC_SCOPE, hvvc_to_bridge.msg_id_panel);
             v_data_slv(v_dut_data_width*(i+1)-1 downto v_dut_data_width*i) := v_sbi_received_data(v_dut_data_width-1 downto 0);
             v_dut_address := v_dut_address + v_dut_address_increment;

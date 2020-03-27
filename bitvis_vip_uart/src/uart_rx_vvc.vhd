00e54bdba155d8732baaa1f18e6dd9220c651f27
Marius Elvegård <marius.elvegard@me.no>
2020-03-27 11:34:34 +0100

    BV_UVVM-879: removed pad_sb_slv() and changed to slv8_sb_pkg in UART VVC

--- bitvis_vip_uart/src/uart_rx_vvc.vhd
+++ bitvis_vip_uart/src/uart_rx_vvc.vhd
@@ -284,7 +284,7 @@ begin
           -- Request SB check result
           if v_cmd.data_routing = TO_SB then
             -- call SB check_received
-            UART_SB.check_received(GC_INSTANCE_IDX, pad_sb_slv(v_read_data(GC_DATA_WIDTH-1 downto 0)));
+            UART_SB.check_received(GC_INSTANCE_IDX, v_read_data(GC_DATA_WIDTH-1 downto 0));
           else
             work.td_vvc_entity_support_pkg.store_result(result_queue => result_queue,
                                                          cmd_idx     => v_cmd.cmd_idx,

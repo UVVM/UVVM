59869c51d8cbed31300db08dddf42fb35200f313
UVVM <info@bitvis.no>
2020-03-30 20:49:43 +0200

    Updated to UVVM version v2 v2020.03.30b - Please see CHANGES.TXT for details.

--- bitvis_vip_sbi/src/sbi_vvc.vhd
+++ bitvis_vip_sbi/src/sbi_vvc.vhd
@@ -286,7 +286,7 @@ begin
             -- Randomise data if applicable
             case v_cmd.randomisation is
               when RANDOM =>
-                v_cmd.data := std_logic_vector(to_unsigned(random(0, 16), v_cmd.data'length)); -- Hard coded for TB example
+                v_cmd.data := std_logic_vector(random(v_cmd.data'length));
               when RANDOM_FAVOUR_EDGES =>
                 null; -- Not implemented yet
               when others => -- NA

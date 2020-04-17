093ba6f659e083917faf5e57343aae28f4af5c3b
Erick Castillo <erick.castillo@bitvis.no>
2020-04-17 10:58:37 +0200

    BV_UVVM-892: Renamed demo tb.

--- bitvis_vip_ethernet/tb/ethernet_sbi_gmii_demo_tb.vhd
+++ bitvis_vip_ethernet/tb/ethernet_sbi_gmii_demo_tb.vhd
@@ -37,11 +37,11 @@ use work.ethernet_mac_pkg.all;
 
 
 -- Test case entity
-entity ethernet_sbi_gmii_tb is
-end entity ethernet_sbi_gmii_tb;
+entity ethernet_sbi_gmii_demo_tb is
+end entity ethernet_sbi_gmii_demo_tb;
 
 -- Test case architecture
-architecture func of ethernet_sbi_gmii_tb is
+architecture func of ethernet_sbi_gmii_demo_tb is
   --------------------------------------------------------------------------------
   -- Types and constants declarations
   --------------------------------------------------------------------------------
@@ -66,7 +66,7 @@ begin
   -----------------------------------------------------------------------------
   -- Instantiate test harness, containing DUT and VVCs
   -----------------------------------------------------------------------------
-  i_test_harness : entity bitvis_vip_ethernet.ethernet_sbi_gmii_th
+  i_test_harness : entity bitvis_vip_ethernet.ethernet_sbi_gmii_demo_th
     generic map(
       GC_CLK_PERIOD => C_CLK_PERIOD
     );

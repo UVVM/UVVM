261e1a091dc7e4f7c755a59b719984f7da424c75
Marius Elvegård <marius.elvegard@me.com>
2020-09-10 16:18:09 +0200

    TEST: updated maintenance script verify_with_golden.py for uvvm_util

--- uvvm_util/script/maintenance_script/verify_with_golden.py
+++ uvvm_util/script/maintenance_script/verify_with_golden.py
@@ -6,22 +6,13 @@ import glob
 from pathlib import Path
 
 
-def cmp_files(sim_file, golden_file):
-  l1 = l2 = ' '
-  with open(sim_file, 'r') as f1, open(golden_file, 'r') as f2:
-    l1 = f1.readline().rstrip()
-    l2 = f2.readline().rstrip()
 
-    while l1 != '' and l2 != '':
-      if l1 != l2:
-        print("ERROR: File "+ sim_file + " was not as expected!")
-        return 1
-      else:
-        l1 = f1.readline().rstrip()
-        l2 = f2.readline().rstrip()
-
-  os.remove(sim_file)
-  return 0
+def get_file_list(path = "."):
+    filelist = []
+    for filename in os.listdir(path):
+      if filename.endswith(".txt"):
+        filelist.append(filename)
+    return filelist
 
 
 def compare(modelsim=False, riviera=False):
@@ -29,29 +20,82 @@ def compare(modelsim=False, riviera=False):
   num_errors = 0
 
   if modelsim:
-    golden_path = sim_path + '/../script/maintenance_script/golden_modelsim/test_output'
+    golden_path = sim_path + '/../script/maintenance_script/golden_modelsim'
   elif riviera:
-    golden_path = sim_path + '/../script/maintenance_script/golden_riviera_pro/test_output'
+    golden_path = sim_path + '/../script/maintenance_script/golden_riviera_pro'
 
+  # Get file lists
+  filelist = get_file_list(".")
+  golden_file_list = get_file_list(golden_path)
 
-  sim_files = [f for f in glob.glob(sim_path + "**/*.txt", recursive=False)]
-  golden_files = [f for f in glob.glob(golden_path + "**/*.txt", recursive=False)]
+  failing_verify_file = []
 
-  if len(sim_files) != len(golden_files):
-    print("ERROR! number of output files and golden files do not match! " + str(len(sim_files)) + " / " + str(len(golden_files)))
-  else:
-    for idx in range(len(golden_files)):
-      print(sim_files[idx] + " <-----> " + golden_files[idx])
-      num_errors += cmp_files(sim_files[idx], golden_files[idx])
+  for filename in filelist:
+      golden_file = golden_path + '/' + filename
+
+      if os.path.isfile(golden_file):
+          golden_file_lines = len(open(golden_file).readlines(  ))
+          check_file_lines  = len(open(filename).readlines(  ))
+            
+          # Read golden file
+          with open(golden_file, 'r') as file:
+              golden_lines = file.readlines()
+
+          # Read verify file 
+          with open(filename, 'r') as file:
+              verify_lines = file.readlines()
+
+          # Compare files
+          error_found = False
+          for idx, line in enumerate(golden_lines):
+              if golden_lines[idx] != verify_lines[idx]:
+                  failing_verify_file.append(filename)
+                  error_found = True
+                  break
+          # Check for line number mismatch
+          if not(error_found) and (golden_file_lines != check_file_lines):
+              failing_verify_file.append(filename)
+              error_found = True
+
+          # Remove OK files
+          if not(error_found):
+              os.remove(filename)   
+
+      else:
+          print("ERROR! Golden do not have file : %s" %(filename))             
+          failing_verify_file.append(filename)
+        
+
 
   if modelsim:
     simulator = "[MODELSIM]"
   elif riviera:
     simulator = "[RIVIERA_PRO]"
 
-  print("%s Number of files with error(s): %d" %(simulator, num_errors))
-  # Return number of failing tests to caller
+  # Present statistics
+  print("%s Number of golden files found : %d" %(simulator, len(golden_file_list)))
+  print("%s Number of verify files found : %d" %(simulator, len(filelist)))
+  print("%s Number of verified files with errors : %d" %(simulator, len(failing_verify_file)))
+
+  # Check that all files have been verified
+  num_missing_files = abs(len(filelist) - len(golden_file_list))
+  if num_missing_files > 0:
+      print("WARNING! Number of files do not match : %d != %d" %(len(filelist), len(golden_file_list)))
+
+  # List files with errors
+  if failing_verify_file:
+      print("Mismatch found in the following file(s) : ")
+      for file in failing_verify_file:
+          print(file)
+
+  # Return the number of errors to caller
+  num_errors = len(failing_verify_file) + num_missing_files
+  if num_errors != 0:
+      print("Golden failed with %d error(s)." %(num_errors))
   sys.exit(num_errors)
+
+
+
 
 
 def main(argv):

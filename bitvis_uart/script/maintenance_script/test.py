b627da4fe0ff22b69018b6ff2fe2d1c6c8bc04e7
Marius Elvegård <marius.elvegard@inventas.no>
2021-04-13 15:08:58 +0200

    TEST: added regression test scripts for hdlunit

--- bitvis_uart/script/maintenance_script/test.py
+++ bitvis_uart/script/maintenance_script/test.py
@@ -7,29 +7,33 @@ except:
     sys.exit(1)
 
     
-print('Verify Bitvis IRQC DUT')
+print('Verify Bitvis UART DUT')
 
-
-hdlunit = HDLUnit()
-hdlunit.set_simulator("GHDL")
-
-# Set testcase detection string
-hdlunit.set_testcase_id("GC_TESTCASE")
+hdlunit = HDLUnit(simulator='modelsim')
 
 # Add util, fw and VIP Scoreboard
 hdlunit.add_files("../../uvvm_util/src/*.vhd", "uvvm_util")
 hdlunit.add_files("../../uvvm_vvc_framework/src/*.vhd", "uvvm_vvc_framework")
 hdlunit.add_files("../../bitvis_vip_scoreboard/src/*.vhd", "bitvis_vip_scoreboard")
+
 # Add other VIPs in the TB
 #  - SBI VIP
 hdlunit.add_files("../../bitvis_vip_sbi/src/*.vhd", "bitvis_vip_sbi")
 hdlunit.add_files("../../uvvm_vvc_framework/src_target_dependent/*.vhd", "bitvis_vip_sbi")
+#  - UART VIP
+hdlunit.add_files("../../bitvis_vip_uart/src/*.vhd", "bitvis_vip_uart")
+hdlunit.add_files("../../uvvm_vvc_framework/src_target_dependent/*.vhd", "bitvis_vip_uart")
+#  - Clock Generator VVC
+hdlunit.add_files("../../bitvis_vip_clock_generator/src/*.vhd", "bitvis_vip_clock_generator")
+hdlunit.add_files("../../uvvm_vvc_framework/src_target_dependent/*.vhd", "bitvis_vip_clock_generator")
 # Add DUT
-hdlunit.add_files("../../bitvis_irqc/src/*.vhd", "bitvis_irqc")
+hdlunit.add_files("../../bitvis_uart/src/*.vhd", "bitvis_uart")
+
 # Add TB/TH
-hdlunit.add_files("../../bitvis_irqc/tb/*.vhd", "bitvis_irqc")
-hdlunit.add_files("../../bitvis_irqc/tb/maintenance_tb/*.vhd", "bitvis_irqc")
-hdlunit.start()
+hdlunit.add_files("../../bitvis_uart/tb/maintenance_tb/*.vhd", "bitvis_uart")
+hdlunit.add_files("../../bitvis_uart/tb/*.vhd", "bitvis_uart")
+
+hdlunit.start(regression_mode=True, gui_mode=False)
 
 
 num_failing_tests = hdlunit.get_num_fail_tests()
@@ -37,5 +41,3 @@ num_failing_tests = hdlunit.get_num_fail_tests()
 hdlunit.check_run_results(exp_fail=0)
 
 sys.exit(num_failing_tests)
-
-
---
+++ bitvis_vip_avalon_st/script/maintenance_script/test.py
@@ -0,0 +1,61 @@
+import sys
+from itertools import product
+
+
+try:
+    from hdlunit import HDLUnit
+except:
+    print('Unable to import HDLUnit module. See HDLUnit documentation for installation instructions.')
+    sys.exit(1)
+
+
+
+# Create testbench configuration with TB generics
+def create_config(channel_widths, data_widths, error_widths):
+    config = []
+    for channel_width, data_width, error_width in product(channel_widths, data_widths, error_widths):
+        config.append([str(channel_width),
+                       str(data_width),
+                       str(error_width)])
+    return config
+
+
+    
+print('Verify Bitvis VIP Avalon ST')
+
+hdlunit = HDLUnit(simulator='modelsim')
+
+# Add util, fw and VIP Scoreboard
+hdlunit.add_files("../../uvvm_util/src/*.vhd", "uvvm_util")
+hdlunit.add_files("../../uvvm_vvc_framework/src/*.vhd", "uvvm_vvc_framework")
+hdlunit.add_files("../../bitvis_vip_scoreboard/src/*.vhd", "bitvis_vip_scoreboard")
+
+# Setup BFM TB test generics
+for config in create_config(channel_widths=[7], data_widths=[8], error_widths=[1]):
+    hdlunit.add_generics("avalon_st_bfm_tb", ["GC_CHANNEL_WIDTH", config[0], "GC_DATA_WIDTH", config[1], "GC_ERROR_WIDTH", config[2]])        
+for config in create_config(channel_widths=[8], data_widths=[16], error_widths=[1]):
+    hdlunit.add_generics("avalon_st_bfm_tb", ["GC_CHANNEL_WIDTH", config[0], "GC_DATA_WIDTH", config[1], "GC_ERROR_WIDTH", config[2]])
+for config in create_config(channel_widths=[8], data_widths=[32], error_widths=[1]):
+    hdlunit.add_generics("avalon_st_bfm_tb", ["GC_CHANNEL_WIDTH", config[0], "GC_DATA_WIDTH", config[1], "GC_ERROR_WIDTH", config[2]])
+# Setup VVC TB test generics
+for config in create_config(channel_widths=[7], data_widths=[8], error_widths=[1]):
+    hdlunit.add_generics("avalon_st_vvc_tb", ["GC_CHANNEL_WIDTH", config[0], "GC_DATA_WIDTH", config[1], "GC_ERROR_WIDTH", config[2]])
+for config in create_config(channel_widths=[8], data_widths=[16], error_widths=[1]):
+    hdlunit.add_generics("avalon_st_vvc_tb", ["GC_CHANNEL_WIDTH", config[0], "GC_DATA_WIDTH", config[1], "GC_ERROR_WIDTH", config[2]])
+for config in create_config(channel_widths=[8], data_widths=[32], error_widths=[1]):
+    hdlunit.add_generics("avalon_st_vvc_tb", ["GC_CHANNEL_WIDTH", config[0], "GC_DATA_WIDTH", config[1], "GC_ERROR_WIDTH", config[2]])
+
+# Add Avalon ST VIP
+hdlunit.add_files("..//src/*.vhd", "bitvis_vip_avalon_st")
+hdlunit.add_files("../../uvvm_vvc_framework/src_target_dependent/*.vhd", "bitvis_vip_avalon_st")
+
+# Add TB/TH
+hdlunit.add_files("../tb/maintenance_tb/*.vhd", "bitvis_vip_avalon_st")
+
+hdlunit.start(regression_mode=True, gui_mode=False)
+
+num_failing_tests = hdlunit.get_num_fail_tests()
+
+hdlunit.check_run_results(exp_fail=0)
+
+sys.exit(num_failing_tests)

fabf4e88b4fd410e3b776c3d95d75c38a5bce70b
Henrik Solvang <hos@spacetec.no>
2021-08-17 08:53:22 +0200

    Implement changes from review.

--- bitvis_vip_axistream/src/axistream_bfm_pkg.vhd
+++ bitvis_vip_axistream/src/axistream_bfm_pkg.vhd
@@ -1573,7 +1573,7 @@ package body axistream_bfm_pkg is
     data_length := v_byte_cnt;
 
     -- Log the received frame
-    log(ID_FRAME_DATA, v_proc_call.all & "=> Rx Frame (" & to_string(v_byte_cnt) & "B) " & to_string(data_array) & ". " & add_msg_delimiter(msg), scope, msg_id_panel);
+    log(ID_PACKET_PAYLOAD, v_proc_call.all & "=> Rx Frame (" & to_string(v_byte_cnt) & "B) " & to_string(data_array) & ". " & add_msg_delimiter(msg), scope, msg_id_panel);
 
     -- Check if there was a timeout or it was successful
     if v_timeout then

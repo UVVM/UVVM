5112802b795dd4e1c2d3f78b7642cf6d4dbf020f
Erick Castillo <erick.castillo@inventas.no>
2021-09-24 14:15:54 +0200

    Updated documentation project name.

--- uvvm_util/doc/rst/conf.py
+++ uvvm_util/doc/rst/conf.py
@@ -17,7 +17,7 @@ import os
 
 # -- Project information -----------------------------------------------------
 
-project = 'Advanced Randomization and Functional Coverage'
+project = 'Randomization and Functional Coverage'
 copyright = '2021, UVVM'
 author = 'UVVM'
 

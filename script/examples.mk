1dc262c3e6ba5cb5ada894be96faf9feba7d217a
Adam Barnes <ambarnes@gmail.com>
2022-09-05 22:39:44 +0100

    Single makefile supports simulating with GHDL, NVC, ModelSim, Questa
    Add makefiles to example design script directories; simulator specific heavy lifting is done in makefile includes located in main script directory.

---
+++ script/examples.mk
@@ -0,0 +1,23 @@
+# examples.mk - an include for makefiles for UVVM examples
+
+ifeq ($(REPO_ROOT),)
+REPO_ROOT=$(shell git rev-parse --show-toplevel)
+endif
+DESIGN_NAME:=$(shell basename `dirname $(shell pwd)`)
+DESIGN_PATH:=$(REPO_ROOT)/$(DESIGN_NAME)
+SCRIPT_PATH:=$(DESIGN_PATH)/script
+DUT_LIB:=$(word 3,$(shell head -n 1 compile_order.txt))
+DUT_FILES:=$(shell tail -n +2 compile_order.txt)
+TB_PATH:=$(DESIGN_PATH)/tb
+TOP=$(notdir $(basename $(shell ls $(TB_PATH)/*tb.vhd)))
+SRC=\
+    $(addprefix $(SCRIPT_PATH)/, $(DUT_FILES)) \
+    $(shell ls $(TB_PATH)/*th.vhd ls 2> /dev/null) \
+    $(shell ls $(TB_PATH)/*tb.vhd)
+
+SIM_VHDL_RELAXED=TRUE
+#SIM_VHDL_SUPPRESS_IEEE_ASSERTS=TRUE
+
+include $(REPO_ROOT)/script/multisim.mk
+$(eval $(call COMPILE,$(DUT_LIB),$(SRC)))
+$(eval $(call RUN,$(TOP),))

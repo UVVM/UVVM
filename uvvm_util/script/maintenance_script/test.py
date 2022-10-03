ff6e6906ea85387c0aa427fc25e09afdb1227ddf
UVVM <info@uvvm.org>
2022-10-03 14:46:53 +0200

    TEST: changed from alias py to python in test.py script for uvvm_util.

--- uvvm_util/script/maintenance_script/test.py
+++ uvvm_util/script/maintenance_script/test.py
@@ -45,32 +45,36 @@ hr.add_files("../../tb/maintenance_tb/*.vhd", "uvvm_util")
 
 # Define testcase names with generics for GC_TESTCASE
 hr.add_generics(entity="generic_queue_array_tb",
-                     generics=["GC_TESTCASE", "generic_queue_array_tb"])
+                generics=["GC_TESTCASE", "generic_queue_array_tb"])
 hr.add_generics(entity="generic_queue_record_tb",
-                     generics=["GC_TESTCASE", "generic_queue_record_tb"])
+                generics=["GC_TESTCASE", "generic_queue_record_tb"])
 hr.add_generics(entity="generic_queue_tb",
-                     generics=["GC_TESTCASE", "generic_queue_tb"])
+                generics=["GC_TESTCASE", "generic_queue_tb"])
 hr.add_generics(entity="simplified_data_queue_tb",
-                     generics=["GC_TESTCASE", "simplified_data_queue_tb"])
+                generics=["GC_TESTCASE", "simplified_data_queue_tb"])
 
 output_path = os_adjust_path(os.getcwd() + '//')
 hr.add_generics(entity='func_cov_tb',
-                     architecture='func',
-                     generics=['GC_FILE_PATH', (output_path, 'PATH')])
+                architecture='func',
+                generics=['GC_FILE_PATH', (output_path, 'PATH')])
 
 hr.start(regression_mode=True, gui_mode=False)
 
 # Run coverage accumulation script
-hr.run_command("py ../../uvvm_util/script/func_cov_merge.py -f db_*_parallel_*.txt -o func_cov_accumulated_verbose.txt -r")
-hr.run_command("py ../../uvvm_util/script/func_cov_merge.py -f db_*_parallel_*.txt -o func_cov_accumulated_non_verbose.txt -r -nv")
-hr.run_command("py ../../uvvm_util/script/func_cov_merge.py -f db_*_parallel_*.txt -o func_cov_accumulated_holes.txt -r -hl -im")
+hr.run_command(
+    "py ../../uvvm_util/script/func_cov_merge.py -f db_*_parallel_*.txt -o func_cov_accumulated_verbose.txt -r")
+hr.run_command(
+    "py ../../uvvm_util/script/func_cov_merge.py -f db_*_parallel_*.txt -o func_cov_accumulated_non_verbose.txt -r -nv")
+hr.run_command(
+    "py ../../uvvm_util/script/func_cov_merge.py -f db_*_parallel_*.txt -o func_cov_accumulated_holes.txt -r -hl -im")
 
 num_failing_tests = hr.get_num_fail_tests()
 num_passing_tests = hr.get_num_pass_tests()
 
 # Check with golden reference, args: 1=modelsim simulator, 2=path to uvvm_util
 path_to_uvvm_util_sim = "./../../uvvm_util/sim"
-(ret_txt, ret_code) = hr.run_command("py ../../uvvm_util/script/maintenance_script/verify_with_golden.py -modelsim %s" % (path_to_uvvm_util_sim))
+(ret_txt, ret_code) = hr.run_command(
+    "python ../../uvvm_util/script/maintenance_script/verify_with_golden.py -modelsim %s" % (path_to_uvvm_util_sim))
 print(ret_txt.replace('\\', '/'))
 
 # Golden compare ok?

58f3b33b0ce6cf679164bd07fed0da5d28fc0725
UVVM <info@bitvis.no>
2023-03-13 12:50:17 +0100

    Merge pull request #189 from amb5l/master
    Additional shell scripts

---
+++ uvvm_util/script/compile_src.sh
@@ -0,0 +1,20 @@
+#!/bin/zsh
+
+# compile_src.sh
+# Compiles UVVM component (library or VIP).
+# Usage: sh compile_src.sh <simulator> <source_dir> <target_dir>
+# simulator   : ghdl, nvc, or vsim
+# target_dir  : path to target directory (optional, defaults to .)
+
+set -o errexit
+SIM=$1
+TARGET_DIR=$2
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+if [ "$OSTYPE" = "msys" ] || [ "$OSTYPE" = "cygwin" ]; then
+    SCRIPT_DIR=$(cygpath -m ${SCRIPT_DIR})
+fi
+UVVM_DIR=$SCRIPT_DIR/../..
+
+source $UVVM_DIR/script/multisim.sh
+
+compile_component $SCRIPT_DIR/..

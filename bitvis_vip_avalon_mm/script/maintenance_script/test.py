d47da24693f69bc49b276ed3e37321dd89a23e5e
Marius Elvegård <marius.elvegard@me.com>
2024-03-04 11:49:28 +0100

    TEST: added altera files to run with all simulators

--- bitvis_vip_avalon_mm/script/maintenance_script/test.py
+++ bitvis_vip_avalon_mm/script/maintenance_script/test.py
@@ -7,17 +7,19 @@ from itertools import product
 try:
     from hdlregression import HDLRegression
 except:
-    print('Unable to import HDLRegression module. See HDLRegression documentation for installation instructions.')
+    print(
+        "Unable to import HDLRegression module. See HDLRegression documentation for installation instructions."
+    )
     sys.exit(1)
 
 
-def cleanup(msg='Cleaning up...'):
+def cleanup(msg="Cleaning up..."):
     print(msg)
 
     sim_path = os.getcwd()
 
     # Check if the current directory is 'sim'
-    if os.path.basename(sim_path) == 'sim':
+    if os.path.basename(sim_path) == "sim":
         for files in os.listdir(sim_path):
             path = os.path.join(sim_path, files)
             try:
@@ -28,39 +30,47 @@ def cleanup(msg='Cleaning up...'):
         print('Current directory is not "sim". Skipping cleanup.')
 
 
-print('Verify Bitvis VIP Avalon MM')
+print("Verify Bitvis VIP Avalon MM")
 
-cleanup('Removing any previous runs.')
+cleanup("Removing any previous runs.")
 
-hr = HDLRegression(simulator='modelsim')
+hr = HDLRegression(simulator="modelsim")
 
 # Add util, fw and VIP Scoreboard
 hr.add_files("../../../uvvm_util/src/*.vhd", "uvvm_util")
 hr.add_files("../../../uvvm_vvc_framework/src/*.vhd", "uvvm_vvc_framework")
-hr.add_files("../../../bitvis_vip_scoreboard/src/*.vhd",
-             "bitvis_vip_scoreboard")
+hr.add_files("../../../bitvis_vip_scoreboard/src/*.vhd", "bitvis_vip_scoreboard")
 
 # Add Avalon MM VIP
 hr.add_files("../../src/*.vhd", "bitvis_vip_avalon_mm")
-hr.add_files("../../../uvvm_vvc_framework/src_target_dependent/*.vhd",
-             "bitvis_vip_avalon_mm")
+hr.add_files(
+    "../../../uvvm_vvc_framework/src_target_dependent/*.vhd", "bitvis_vip_avalon_mm"
+)
 
 # Add TB/TH etc
 hr.add_files("../../tb/maintenance_tb/*.vhd", "bitvis_vip_avalon_mm")
 
-hr.add_generics(entity="avalon_mm_vvc_pipeline_tb",
-                architecture=None,
-                generics=["GC_DELTA_DELAYED_VVC_CLK", True])
+hr.add_files("../../tb/maintenance_tb/altera_files/altera/*.vhd", "altera")
+hr.add_files("../../tb/maintenance_tb/altera_files/altera_mf/*.vhd", "altera_mf")
+hr.add_files("../../tb/maintenance_tb/altera_files/220model/*.vhd", "lpm")
 
-hr.add_generics(entity="avalon_mm_vvc_pipeline_tb",
-                architecture=None,
-                generics=["GC_DELTA_DELAYED_VVC_CLK", False])
+hr.add_generics(
+    entity="avalon_mm_vvc_pipeline_tb",
+    architecture=None,
+    generics=["GC_DELTA_DELAYED_VVC_CLK", True],
+)
+
+hr.add_generics(
+    entity="avalon_mm_vvc_pipeline_tb",
+    architecture=None,
+    generics=["GC_DELTA_DELAYED_VVC_CLK", False],
+)
 
 sim_options = None
 default_options = []
 simulator_name = hr.settings.get_simulator_name()
-if simulator_name in ['MODELSIM', 'RIVIERA']:
-    sim_options = '-t ns'
+if simulator_name in ["MODELSIM", "RIVIERA"]:
+    sim_options = "-t ns"
     # Set compile options
     default_options = ["-suppress", "1346,1246,1236", "-2008"]
     hr.set_simulator(simulator=simulator_name, com_options=default_options)
@@ -75,6 +85,6 @@ if num_passing_tests == 0:
     sys.exit(1)
 # Remove output only if OK
 if hr.check_run_results(exp_fail=0) is True:
-    cleanup('Removing simulation output')
+    cleanup("Removing simulation output")
 # Return number of failing tests
 sys.exit(num_failing_tests)
